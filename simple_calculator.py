773affe890a9d09d10f7790981f9c27292cdfb9b
angelalucban07-bit <angelalucban07@gmail.com>
2026-04-29 23:47:00 +0800

    f: initialize class structure then add add subtract methods.

---
+++ simple_calculator.py
@@ -0,0 +1,6 @@
+class Calculator:
+    def addition(self, first_n, second_n):
+        return first_n + second_n
+    def subtraction(self, first_n, second_n):
+        return first_n - second_n
+    #will add subtract, multiply, and divide methods here...
