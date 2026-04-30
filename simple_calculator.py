31ad084296138202cd720d55bdfdbaef52fa4366
angelalucban07-bit <angelalucban07@gmail.com>
2026-05-01 00:14:13 +0800

    add: menu choice validation for display menu. no functions yet.

--- simple_calculator.py
+++ simple_calculator.py
@@ -8,7 +8,7 @@ class BaseCalculator:
                 return number1, number2
             except ValueError:
                 print("Invalid input. Please Enter numbers only.")
-
+#child class
 class MathOperations(BaseCalculator):
     def addition(self, number1, number2):
         return number1 + number2
@@ -17,6 +17,28 @@ class MathOperations(BaseCalculator):
     def multiplication(self, number1, number2):
         return number1 * number2
     def division(self, number1, number2):
-        return number1/ number2
+        try:
+            return number1 / number2
+        except ZeroDivisionError:
+        print("Error: Can't divide by zero")
 
-    #will add subtract, multiply, and divide methods here...
+#another class for the main loop to display the menu
+class CalculatorInterface:
+    def __init__(self):
+        self.operations = MathOperations()
+
+    def run(self):
+        while True:
+            print("\n=== Calculator ===")
+            print("1. Addition")
+            print("2. Subtraction")
+            print("3. Multiplication")
+            print("4. Division")
+
+            choice = input("Enter your choice: ")
+
+            if choice not in ["1", "2", "3", "4"]:
+                print("Invalid input. Please Enter only one of the following values (1-4):")
+                continue
+#di muna tapos xori po
+#will learn better methods for better main display
