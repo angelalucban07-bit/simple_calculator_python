97e5c86031fe79bd4b9255004d34cebff13fdfd7
angelalucban07-bit <angelalucban07@gmail.com>
2026-04-30 23:50:45 +0800

    add: parent class with input and made a different class operations as a parameter.

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,6 +1,22 @@
-class Calculator:
-    def addition(self, first_n, second_n):
-        return first_n + second_n
-    def subtraction(self, first_n, second_n):
-        return first_n - second_n
+#parent class
+class lculator:
+    def get_numbers(self):
+        while True:
+            try:
+                number1 = int(input("Enter a number: "))
+                number2 = int(input("Enter another number: "))
+                return number1, number2
+            except ValueError:
+                print("Invalid input. Please Enter numbers only.")
+
+class Operation(lculator):
+    def addition(self, number1, number2):
+        return number1 + number2
+    def subtraction(self, number1, number2):
+        return number1 - number2
+    def multiplication(self, number1, number2):
+        return number1 * number2
+    def division(self, number1, number2):
+        return number1/ number2
+
     #will add subtract, multiply, and divide methods here...
